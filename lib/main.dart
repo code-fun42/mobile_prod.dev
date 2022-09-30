d92a636ed3b63a6da1414e5470ed43112dbcedb3
ilya k <ilya.k423@gmail.com>
2022-09-30 20:38:04 +0300

    start creation of the sctructure in /lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/startDisplay.dart';
 
 void main() {
   runApp(
@@ -7,27 +8,4 @@ void main() {
       home: StartDisplayLoad(),
     ),
   );
-}
-
-class StartDisplayLoad extends StatelessWidget {
-  const StartDisplayLoad({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      // body is the majority of the screen.
-      body: Center(
-
-        child: Text(
-          '< TITLE />',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Colors.red
-          ),
-        ),
-
-      ),
-    );
-  }
 }
---
+++ lib/src/startDisplay.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+export 'startDisplay.dart';
+
+class StartDisplayLoad extends StatelessWidget {
+  const StartDisplayLoad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // body is the majority of the screen.
+      body: Center(
+
+        child: Text(
+          '< TITLE />',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.red
+          ),
+        ),
+
+      ),
+    );
+  }
+}
